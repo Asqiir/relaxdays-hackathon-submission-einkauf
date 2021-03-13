d3e1b86e43263efb0c733a1ed2042c0b6067d67e
asqiir <asqiir@blaupost.de>
2021-03-13 15:50:06 +0100

    update 3, aber nicht 2

--- server.py
+++ server.py
@@ -4,7 +4,11 @@
 from bottle import request, response, run, post, get, put, delete, route
 import json
 import yaml
+import datetime
 
+
+def convert_to_en_format(x): #assumes %d.%m.%Y %H:%M:%S input, returns %Y.%m.%d %H:%M:%S output
+	return x[6:10] + '-' + x[3:5] + '-' + x[0:2] + ' ' + x[11:19]
 
 class Verwalter:
 	data = []
@@ -13,11 +17,15 @@ class Verwalter:
 	def __init__(self):
 		self.__write()
 
+	def __sort(self):
+		self.data_sorted_by_date = sorted(self.data, key=lambda k:k['time'])
+
 	def __write(self):
 		with open(self.json_file,'w') as file:
 			file.write(json.dumps(self.data))
 
 	def add(self,einkauf):
+		einkauf['time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 		self.data.append(einkauf)
 		self.__write()
 
@@ -26,6 +34,10 @@ class Verwalter:
 			return self.data
 		else:
 			return list(filter(lambda k:k['articleID']==article_id, self.data))
+
+	def getByDate(self, after=None, before=None):
+		return list(filter(lambda k:(after==None or after<=k['time']) and (before==None or before>=k['time']), self.data))
+
 
 verwalter=Verwalter()
 
@@ -46,12 +58,21 @@ def purchaseForArticle():
 	response.headers['Content-Type'] = 'application/json'
 	return json.dumps(verwalter.getAll(article_id=articleID))
 
+@get('/purchasesBetween')
+def purchasesBetween():
+	x = request.query['x'][1:-1]
+	y = request.query['y'][1:-1]
+
+	x_en = convert_to_en_format(x)
+	y_en = convert_to_en_format(y)
+
+	return json.dumps(verwalter.getByDate(after=x_en,before=y_en))
+
 
 global swagger_json
 swagger_json = None
 with open('swagger.yaml') as file:
 	swagger_json = yaml.safe_load(file)
-	#bottle.install(SwaggerPlugin(swagger_def))
 
 @route('/swagger')
 def swagger():
--- test.py
+++ test.py
@@ -4,6 +4,11 @@ import json
 def compare_lists(list1, list2): 
     return sorted(list1, key=lambda k:(k['lieferant'],k['articleID'],k['menge']))==sorted(list2, key=lambda k:(k['lieferant'],k['articleID'],k['menge']))
 
+def nodate(x):
+	for entry in x:
+		del entry['time']
+	return x
+
 o1 = {'lieferant':'x','articleID':3,'menge':10}
 o2 = {'lieferant':'x','articleID':3,'menge':10}
 o3 = {'lieferant':'y','articleID':3,'menge':10}
@@ -15,7 +20,7 @@ requests.post("http://0.0.0.0:8080/purchase", data=json.dumps(o3))
 requests.post("http://0.0.0.0:8080/purchase", data=json.dumps(o4))
 
 with open('data.json','r') as file:
-	data = json.loads(file.read())
+	data = nodate(json.loads(file.read()))
 	if not compare_lists(data, [o1,o2,o3,o4]):
 		print(data)
 		print('POST wrong')
@@ -23,7 +28,7 @@ with open('data.json','r') as file:
 r = requests.get("http://0.0.0.0:8080/purchases")
 
 with open('data.json','r') as file:
-	data = r.json()
+	data = nodate(r.json())
 	if not compare_lists(data, [o1,o2,o3,o4]):
 		print(data)
 		print('GET ALL wrong')
@@ -33,15 +38,15 @@ r2 = requests.get("http://0.0.0.0:8080/purchaseForArticle", params={'x':7})
 r3 = requests.get("http://0.0.0.0:8080/purchaseForArticle", params={'x':0})
 
 with open('data.json','r') as file:
-	data = r1.json()
+	data = nodate(r1.json())
 	if not compare_lists(data, [o1,o2,o3]):
 		print(data)
 		print('GET ALL 1 wrong')
-	data = r2.json()
+	data = nodate(r2.json())
 	if not compare_lists(data, [o4]):
 		print(data)
 		print('GET ALL 2 wrong')
-	data = r3.json()
+	data = nodate(r3.json())
 	if not compare_lists(data, []):
 		print(data)
 		print('GET ALL 3 wrong')
